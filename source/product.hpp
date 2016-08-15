6725bb3e7f6c979cd4cc280caeaf5ef63e222237
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2016-08-15 15:29:49 +1200

    Defined betting products.

---
+++ source/product.hpp
@@ -0,0 +1,27 @@
+
+#ifndef _GAMBLING_PRODUCT_H
+#define _GAMBLING_PRODUCT_H 1
+
+//System Includes
+
+//Project Includes
+
+//External Includes
+
+//System Namespaces
+
+//Project Namespaces
+
+//External Namespaces
+
+namespace gambling
+{
+    enum Product : uint8_t
+    {
+        NONE = 0,
+        WIN = 1,
+        PLACE = 2
+    };
+}
+
+#endif  /* _GAMBLING_PRODUCT_H */
